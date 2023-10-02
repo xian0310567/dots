8d9a61fb56734d15b2ae589e3e86174f5400e4ae
JeongHan Lee <the_comma2537@naver.com>
2023-10-02 22:40:39 +0900

    Feat: AddEvent modal 추가

--- src/components/layout/Navigator/Navigator.tsx
+++ src/components/layout/Navigator/Navigator.tsx
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from '@/page/Home';
+import AddEvent from '@/modal/AddEvent';
 
 const Navigator = () => {
   const Stack = createNativeStackNavigator();
@@ -15,6 +16,11 @@ const Navigator = () => {
           name="Home"
           component={Home}
           options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="addEvent"
+          component={AddEvent}
+          options={{presentation: 'modal', headerShown: false}}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/modal/AddEvent/AddEvent.tsx
@@ -0,0 +1,7 @@
+import {View} from 'react-native';
+
+const AddEvent = () => {
+  return <View></View>;
+};
+
+export default AddEvent;
