7c6d79ffd77e0498a50480777c9f4cd98f26cc0b
JeongHan Lee <the_comma2537@naver.com>
2023-10-09 23:55:58 +0900

    Style: Form Radius 변경

--- src/components/atom/Form/lib/styled.ts
+++ src/components/atom/Form/lib/styled.ts
@@ -2,7 +2,7 @@ import {StyleSheet} from 'react-native';
 
 const styled = StyleSheet.create({
   container: {
-    borderRadius: 5,
+    borderRadius: 10,
     backgroundColor: '#3a3a3a',
     overflow: 'hidden',
   },
