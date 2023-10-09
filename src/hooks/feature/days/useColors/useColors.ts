924108ba93f335e8cf78e482756cf71fcb8ce258
JeongHan Lee <the_comma2537@naver.com>
2023-10-09 23:43:23 +0900

    Feat: colorPicker 기본 컬러 추가

---
+++ src/hooks/feature/days/useColors/useColors.ts
@@ -0,0 +1,24 @@
+const useColors = () => {
+  const colorList = [
+    {
+      name: 'peach',
+      color: '#F4BFBF',
+    },
+    {
+      name: 'beige',
+      color: '#FFD9C0',
+    },
+    {
+      name: 'cream',
+      color: '#FAF0D7',
+    },
+    {
+      name: 'sea',
+      color: '#8CC0DE',
+    },
+  ];
+
+  return {colorList};
+};
+
+export default useColors;
