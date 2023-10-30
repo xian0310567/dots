149f18c2f04c88ac669b6c632a0f5d4f2a902f3b
JeongHan Lee <the_comma2537@naver.com>
2023-10-30 21:45:04 +0900

    Fix: Header margin 조정

--- src/components/feature/Home/Header/lib/styled.ts
+++ src/components/feature/Home/Header/lib/styled.ts
@@ -3,10 +3,11 @@ import {StyleSheet} from 'react-native';
 const styled = () => {
   return StyleSheet.create({
     space: {
-      height: 40,
+      height: 35,
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'space-between',
+      marginTop: 15,
       // borderBottomWidth: 0.5,
       // borderBottomColor: '#aaa',
     },
