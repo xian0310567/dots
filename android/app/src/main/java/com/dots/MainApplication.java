874420e52520395c1db63b0879628b26ceb3aeb9
JeongHan Lee <the_comma2537@naver.com>
2023-10-30 22:56:35 +0900

    Feat: lottie loader 추가

--- android/app/src/main/java/com/dots/MainApplication.java
+++ android/app/src/main/java/com/dots/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
@@ -21,6 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+          packages.add(new LottiePackage());
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
