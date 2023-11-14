e9e31183ad72cfae1a72586b219198f1ce5e6d41
JeongHan Lee <the_comma2537@naver.com>
2023-11-15 00:59:30 +0900

    Feat: 웹뷰 기능 추가

--- src/page/Nofitication/Nofitication.tsx
+++ src/page/Nofitication/Nofitication.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import Container from '@/components/atom/Container';
+import WebView from 'react-native-webview';
 
 const Nofitication = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <WebView source={{uri: 'http://localhost:3000/'}} />
+    </Container>
+  );
 };
 
 export default Nofitication;
