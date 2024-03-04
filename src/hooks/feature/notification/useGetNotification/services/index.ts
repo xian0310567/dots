d8bbfc15fca759a91c2af75e7e686d444259f065
Jeonghan Lee <the_comma2537@naver.com>
2024-03-05 01:53:48 +0900

    Fix: supabase env test

--- src/hooks/feature/notification/useGetNotification/services/index.ts
+++ src/hooks/feature/notification/useGetNotification/services/index.ts
@@ -1,3 +1,5 @@
+import {useEffect} from 'react';
+
 import {GetNotificationStateCallback} from '../types/useGetNotificationState.types';
 import {GetNotificationServiceCallback} from '../types/useGetNotificationService.types';
 
@@ -7,6 +9,10 @@ const index = (
   state: GetNotificationStateCallback,
 ): GetNotificationServiceCallback => {
   const {getNotice} = getNoticeService(state);
+
+  useEffect(() => {
+    getNotice();
+  }, []);
 
   return {getNotice};
 };
--- src/page/Nofitication/Nofitication.tsx
+++ src/page/Nofitication/Nofitication.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 import Container from '@/components/atom/Container';
 import NotiBoard from '@/components/feature/notification/NotiBoard';
 
+import useGetNotification from '@/hooks/feature/notification/useGetNotification';
+
 const Nofitication = () => {
+  // const notice = useGetNotification();
+
+  console.log(process.env.SUPABASE_URL);
+
   return (
     <Container>
       <NotiBoard></NotiBoard>
