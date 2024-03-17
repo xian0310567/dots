9a1fdbc11be9866066ab23d43f4569491c853b8f
Jeonghan Lee <the_comma2537@naver.com>
2024-03-17 20:05:12 +0900

    Feat: ImageGrid 추가

---
+++ src/components/feature/Home/ImageGrid/ImageGrid.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Container} from './ImageGrid.styled';
+
+const ImageGrid = () => {
+  return <Container></Container>;
+};
+
+export default ImageGrid;
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from '@/components/feature/Home/Header';
 import Container from '@/components/atom/Container';
+import ImageGrid from '@/components/feature/Home/ImageGrid';
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <ImageGrid />
     </Container>
   );
 };
